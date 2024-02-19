27682ce68abb6e66ea82dfda82eca5f8cb260299
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-02-19 16:27:08 +0530

    adeed

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const app = express();
+const { join } = require('node:path');
+const server = http.createServer(app);
+const io = socket(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, './index.html'));
+  });  
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    io.emit("check","somebody came")
+    socket.on("message", (message) => {
+        console.log("Received message:", message);
+        io.emit("show", message);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
